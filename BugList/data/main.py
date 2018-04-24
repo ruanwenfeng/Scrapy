ba1760ac78dfa4279becc348ea4ee7d7d47f67eb
ruanwenfeng <791113844@qq.com>
2018-04-24 10:43:11 +0800

    data change
    Signed-off-by: ruanwenfeng <ruanwenfeng@hotmail.com>

--- BugList/data/main.py
+++ BugList/data/main.py
@@ -1,1 +1,35 @@
-import json, os
+import json, os, csv, re
+with open('./items.json', 'w') as json_data:
+    with open('./final_df.csv', 'r') as f:
+        csv_reader = csv.reader(f)
+        next(csv_reader)
+        for row in csv_reader:
+            """
+            {
+                "id": 39712, 
+                "desc": "e need to confirm this bug with Microsoft.", 
+                "blocked": [], 
+                "dependson": [], 
+                "assigned": "Wan-Teh Chang", 
+                "reporter": "Wan-Teh Chang", 
+                "duplicates": []
+            }
+            """
+            _data = {
+                "id": int(row[0]),
+                "desc": row[1],
+                "blocked": [int(i) for _id in row[9].split(',')
+                            for i in re.findall(r"^\d+$", _id.strip().replace("\n", ""))],
+                "dependson": [int(i) for _id in row[8].split(',')
+                              for i in re.findall(r"^\d+$", _id.strip().replace("\n", ""))],
+                "assigned": row[2].strip().replace("\n", ""),
+                "reporter": row[3].strip().replace("\n", ""),
+                "duplicates": [],
+                "cc": row[4].strip().replace("\n", ""),
+                "platform": row[6].strip().replace("\n", ""),
+                "sys": row[7].strip().replace("\n", ""),
+                "creation_ts": row[11].strip().replace("\n", ""),
+                "modified": row[12].strip().replace("\n", ""),
+                "commname": row[13].strip().replace("\n", "")
+            }
+            json_data.write(json.dumps(_data)+'\n')
--- BugList/dot/main.py
+++ BugList/dot/main.py
@@ -11,6 +11,31 @@ def write_assigned(graph, bug_id, assigned):
     graph.write(' bug_{0} -> "{1}" [label="{2}"];\n'.format(bug_id, assigned, 'assigned'))
 
 
+def write_cc(graph, bug_id, cc):
+    graph.write(' "{0}" [style=filled color=".3 .7 1.0"];\n'.format(cc))
+    graph.write(' bug_{0} -> "{1}" [label="{2}"];\n'.format(bug_id, cc, 'cc'))
+
+
+def write_platform(graph, bug_id, platform):
+    graph.write(' "{0}" [style=filled color=".3 .3 1.0"];\n'.format(platform))
+    graph.write(' bug_{0} -> "{1}" [label="{2}"];\n'.format(bug_id, platform, 'platform'))
+
+
+def write_sys(graph, bug_id, sys):
+    graph.write(' "{0}" [style=filled color=".3 1.0 0.3"];\n'.format(sys))
+    graph.write(' bug_{0} -> "{1}" [label="{2}"];\n'.format(bug_id, sys, 'sys'))
+
+
+def write_creation_ts(graph, bug_id, creation_ts):
+    graph.write(' "{0}" [style=filled color=".3 1.0 0.7"];\n'.format(creation_ts))
+    graph.write(' bug_{0} -> "{1}" [label="{2}"];\n'.format(bug_id, creation_ts, 'creation_ts'))
+
+
+def write_modified(graph, bug_id, modified):
+    graph.write(' "{0}" [style=filled color=".3 0.5 2"];\n'.format(modified))
+    graph.write(' bug_{0} -> "{1}" [label="{2}"];\n'.format(bug_id, modified, 'modified'))
+
+
 def write_duplicate(graph, bug_id, duplicate):
     graph.write(' bug_{0} [shape=circle];\n'.format(duplicate))
     graph.write(' bug_{0} -> bug_{1} [label="{2}"];\n'.format(bug_id, duplicate, 'duplicate'))
@@ -30,6 +55,16 @@ def write_bug(graph, bug_id):
     graph.write(' bug_{0} [shape=circle];\n'.format(bug_id))
 
 
+def write_attr(graph, bug_id, bug_data):
+    write_reporter(graph, bug_id, bug_data['reporter'])
+    write_assigned(graph, bug_id, bug_data['assigned'])
+    write_cc(graph, bug_id, bug_data['cc'])
+    write_platform(graph, bug_id, bug_data['platform'])
+    write_sys(graph, bug_id, bug_data['sys'])
+    write_creation_ts(graph, bug_id, bug_data['creation_ts'])
+    write_modified(graph, bug_id, bug_data['modified'])
+
+
 def main():
     _set = {}
     with open('items.json', 'r') as data:
@@ -41,6 +76,11 @@ def main():
                 'flag': False,
                 'reporter': _json['reporter'],
                 'assigned': _json['assigned'],
+                'cc': _json['cc'],
+                'platform': _json['platform'],
+                'sys': _json['sys'],
+                'creation_ts': _json['creation_ts'],
+                'modified': _json['modified'],
                 'blocked': _json['blocked']
 
             }
@@ -55,8 +95,9 @@ def main():
                 _index = e.pop()
                 if _index in _set.keys():
                     write_bug(graph, _index)
-                    write_reporter(graph, _index, _set[_index]['reporter'])
-                    write_assigned(graph, _index, _set[_index]['assigned'])
+                    # write_reporter(graph, _index, _set[_index]['reporter'])
+                    # write_assigned(graph, _index, _set[_index]['assigned'])
+                    write_attr(graph, _index, _set[_index])
                     r.add(_index)
                     if not _set[_index]['flag']:
                         _duplicates = _set[_index]['duplicates']
@@ -69,20 +110,23 @@ def main():
                         for _duplicate in _duplicates:
                             write_duplicate(graph, _index, _duplicate)
                             if _duplicate in _set.keys():
-                                write_reporter(graph, _duplicate, _set[_duplicate]['reporter'])
-                                write_assigned(graph, _duplicate, _set[_duplicate]['assigned'])
+                                write_attr(graph, _duplicate, _set[_duplicate])
+                                # write_reporter(graph, _duplicate, _set[_duplicate]['reporter'])
+                                # write_assigned(graph, _duplicate, _set[_duplicate]['assigned'])
                             flag = True
                         for _depend in _dependson:
                             write_dependson(graph, _index, _depend)
                             if _depend in _set.keys():
-                                write_reporter(graph, _depend, _set[_depend]['reporter'])
-                                write_assigned(graph, _depend, _set[_depend]['assigned'])
+                                write_attr(graph, _depend, _set[_depend])
+                                # write_reporter(graph, _depend, _set[_depend]['reporter'])
+                                # write_assigned(graph, _depend, _set[_depend]['assigned'])
                             flag = True
                         for _block in _blocked:
                             write_blocked(graph, _index, _block)
                             if _block in _set.keys():
-                                write_reporter(graph, _block, _set[_block]['reporter'])
-                                write_assigned(graph, _block, _set[_block]['assigned'])
+                                write_attr(graph, _block, _set[_block])
+                                # write_reporter(graph, _block, _set[_block]['reporter'])
+                                # write_assigned(graph, _block, _set[_block]['assigned'])
                             flag = True
                 else:
                     r.add(_index)
